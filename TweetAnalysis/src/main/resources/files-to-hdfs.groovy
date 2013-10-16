368ac82cdaaa092e226986d7086c249b59846089
kailashjoshi <kailashrajjoshi@gmail.com>
2013-10-15 23:30:58 -0600

    Update files-to-hdfs.groovy

--- TweetAnalysis/src/main/resources/files-to-hdfs.groovy
+++ TweetAnalysis/src/main/resources/files-to-hdfs.groovy
@@ -2,6 +2,11 @@ if (!fsh.test(inputDir)) {
    fsh.mkdir(inputDir); 
    fsh.copyFromLocal(localSourceFile, inputDir); 
    fsh.chmod(700, inputDir)
+}else{
+   fsh.rmr(inputDir)
+   fsh.mkdir(inputDir); 
+   fsh.copyFromLocal(localSourceFile, inputDir); 
+   fsh.chmod(700, inputDir)
 }
 if (fsh.test(outputDir)) {
    fsh.rmr(outputDir)
